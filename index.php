bff1a632032bbe961fefe27e408edf3d389f0475
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-20 18:27:42 +0100

    FEAT style table + text align

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>PHP Hotel</title>
 </head>
-<body>
+<body class="p-3">
     <h1>Hotels:</h1>
     <?php
 
@@ -49,14 +49,14 @@
             ],
         ];
     ?>
-    <table class="table">
+    <table class="table border border-secondary-subtle">
         <thead>
             <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Descrizione</th>
-                <th scope="col">Parcheggio</th>
-                <th scope="col">Voto</th>
-                <th scope="col">Distanza dal centro</th>
+                <th scope="col" class="text-center">Name</th>
+                <th scope="col" class="text-center">Descrizione</th>
+                <th scope="col" class="text-center">Parcheggio</th>
+                <th scope="col" class="text-center">Voto</th>
+                <th scope="col" class="text-center">Distanza dal centro</th>
             </tr>
         </thead>
         <tbody>
@@ -69,7 +69,7 @@
                     foreach($hotel as $hotelFeatures):
                 ?>
                 <!-- creo una colonna per ogni caratteristica dell'hotel -->
-                <td><?php echo $hotelFeatures;?></td>
+                <td class="text-center"><?php echo $hotelFeatures;?></td>
                 <?php
                     endforeach;
                 ?>
